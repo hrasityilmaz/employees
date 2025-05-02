c96906d93a0c4bbaa1f6dc59b379689311f753bc
hrasity <hrasity@hotmail.com>
2025-05-02 21:08:07 +0300

    init

---
+++ src/test/java/art/timestart/spring/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package art.timestart.spring.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
